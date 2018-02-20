b59f77f701ae55efa817c1c60be67cec3ebc64f0
milad <miladonlinemail@gmail.com>
2018-02-20 10:46:04 -0800

    Add answer model to admin panel

--- rahbord/products/admin.py
+++ rahbord/products/admin.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Question, Group, Registration, Sku
+from .models import Question, Group, Registration, Sku, Answer
 
 admin.site.register(Question)
 admin.site.register(Group)
 admin.site.register(Registration)
 admin.site.register(Sku)
+admin.site.register(Answer)
 
