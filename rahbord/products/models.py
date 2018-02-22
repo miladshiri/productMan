2ea32ad03db94ba1630882e8984f7c3c812fd0c6
milad <miladonlinemail@gmail.com>
2018-02-22 10:31:02 -0800

    model representation in admin panel

--- rahbord/products/models.py
+++ rahbord/products/models.py
@@ -23,14 +23,23 @@ class Registration(TimeStampedModel):
     start_loc_lat = models.FloatField(blank=True, null=True)
     start_loc_long = models.FloatField(blank=True, null=True)
 
+    def __str__(self):
+        return self.questioner_first_name + ' ' + self.questioner_last_name
+
 
 class Question(TimeStampedModel):
     title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 
 class Group(TimeStampedModel):
     title = models.CharField(max_length=100)
     questions = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.title
 
 
 class Sku(TimeStampedModel):
@@ -47,13 +56,20 @@ class Sku(TimeStampedModel):
     sent_loc_long = models.FloatField(blank=True, null=True)
     registration = models.ForeignKey(Registration, blank=True, null=True)
 
+    def __str__(self):
+        return self.title + ' ' + self.hash_code
+
 
 class Answer(TimeStampedModel):
     sku = models.ForeignKey(Sku, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     response = models.CharField(max_length=100, blank=True, null=True)
 
+    def __str__(self):
+        return self.response[:20]
+
 
 class SkuImage(TimeStampedModel):
     sku = models.ForeignKey(Sku, on_delete=models.CASCADE)
     image = models.ImageField()
+
